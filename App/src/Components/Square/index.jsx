f0a58e7b98484a95ce0c5f3bca335898d4c1302e
Renato Silvestre <silvestrerenato17@gmail.com>
2023-11-27 20:05:53 -0400

    Adding the Board and Square Components

--- App/src/Components/Square/index.jsx
+++ App/src/Components/Square/index.jsx
@@ -1,5 +1,7 @@
-export default function Square() {
+import styles from "./styles.module.css"
+
+export default function Square({ value, onSquareClick }) {
   return (
-    <></>
+    <button className={styles.wrapper} onClick={onSquareClick}>{value}</button>
   )
 }
